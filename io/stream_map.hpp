8f036927d177a49bab298ce2427adc894f1215e6
Michele Schimd <skimmoz@gmail.com>
2019-09-05 15:16:38 +0200

    [IO] improved parsing of streams. Added factory functions

--- io/stream_map.hpp
+++ io/stream_map.hpp
@@ -14,12 +14,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// \file stream_map.hpp \brief This file contains the functions to
+/// convert streams to map of key value pairs. It also contains
+/// factory function allowing working directly with file
+
 #include "../ctl.h"
 
+#include <map>
 #include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <map>
+#include <fstream>
 
 #ifndef _CTL_STREAM_MAP_
 #define _CTL_STREAM_MAP_
@@ -27,6 +32,8 @@
 
 CTL_DEFAULT_NAMESPACE_BEGIN
 
+/// \fn _T from_string(std::string str)
+/// \brief converts a string to type _T using operator >>.
 template <typename _T>
 _T from_string(std::string str) {
   std::stringstream _stream{ str };
@@ -42,29 +49,45 @@ from_string<std::string>(std::string str) {
   return  str;
 }
 
+// Tokenization: assumes 'Key [sep] Val' other occurrences of [sep]
+// are considered part of Val
 template <typename _KeyT, typename _ValT>
 std::map<_KeyT,_ValT>
-stream_to_map(std::istream& _is, char delimiter=',', char comment='#') {
+stream_to_map(std::istream& _is, char delimiter='=', char comment='#') {
   std::map<_KeyT, _ValT> _map;
   while(!_is.eof()) {
     std::string line {};
     std::getline(_is, line);
-    // empty lines and comment lines are allowed
-    if (line.empty() || line[0]==comment) {
-      continue;
+    // strip comment setting end to the first occurence of the comment char
+    auto end = std::find(line.begin(), line.end(), comment);
+    auto _sep_iter = std::find(line.begin(), end, delimiter);
+    if (_sep_iter != end) {
+      _KeyT _key = from_string<_KeyT>(std::string {line.begin(), _sep_iter});
+      _ValT _val = from_string<_ValT>(std::string { ++_sep_iter, end});
+      _map[_key] = _val;
     }
-    // TODO: Add strip of any 'inline' comment
-    // Tokenization: assumes 'Key [sep] Val' other occurrences of [sep]
-    // are considered part of Val
-    auto _sep_iter = std::find(line.begin(), line.end(), delimiter);
-    _KeyT _key = from_string<_KeyT>(std::string {line.begin(), _sep_iter});
-    _ValT _val = from_string<_ValT>(std::string { ++_sep_iter, line.end()});
-    _map[_key] = _val;
   }
   return _map;
 }
 
 using StringStringMap = typename std::map<std::string, std::string>;
+
+/// \brief Creates a map from string to string starting from the file
+/// at the given path.
+template <typename _PathT>
+std::map<std::string, std::string>
+make_string_map_from_file(const _PathT& path, char delimiter='=', char comment='#') {
+  std::ifstream ifs {path};
+  return stream_to_map<std::string, std::string>(ifs, delimiter, comment);
+}
+
+/// \brief Creates a map from string to string starting from the given stream
+template <typename _StreamT>
+std::map<std::string, std::string>
+make_string_map_from_stream(_StreamT& ifs, char delimiter='=', char comment='#') {
+  return stream_to_map<std::string, std::string>(ifs, delimiter, comment);
+}
+
 
 CTL_DEFAULT_NAMESPACE_END
 
