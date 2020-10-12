2985745e1ec5e51e05d1c5256a0f5c12f10eb48c
Michele Schimd <skimmoz@gmail.com>
2020-10-12 10:48:23 +0200

    Added bandwise algorithm. Changes in ctl for dynamic programming structures.

--- data_structure/matrix.hpp
+++ data_structure/matrix.hpp
@@ -28,167 +28,167 @@ CTL_DEFAULT_NAMESPACE_BEGIN
 // List of TODOs:
 //  - Make all structures as compliant as possible with the stl
 
-template <typename T_>
-T_**
-alloc_matrix(size_t n, size_t m)
-{
-  T_** p_ = new T_*[n];
-  for (size_t i = 0; i < n; ++i) {
-    p_[i] = new T_[m];
-  }
-  return p_;
+template<typename T_>
+T_ **
+alloc_matrix(size_t n, size_t m) {
+    T_ **p_ = new T_ *[n];
+    for (size_t i = 0; i < n; ++i) {
+        p_[i] = new T_[m];
+    }
+    return p_;
 }
 
-template <typename T_>
+template<typename T_>
 void
-free_matrix(T_** p_, size_t n, size_t m)
-{
-  for (size_t i = 0; i < n; ++i) {
-    delete[] p_[i];
-  }
-  delete[] p_;
+free_matrix(T_ **p_, size_t n, size_t m) {
+    for (size_t i = 0; i < n; ++i) {
+        delete[] p_[i];
+    }
+    delete[] p_;
 }
 
 // TODO: Improve by substituting double pointer with a single pointer
 // array. Then use the same structure as _2D_matrix making the access
 // through the array of pointers. This could increase efficiency since
 // the items are stored in a one dimension array.
-template <typename _ContentT, typename _Alloc = std::allocator<_ContentT>>
-class _2D_array
-{
+template<typename _ContentT, typename _Alloc = std::allocator<_ContentT>>
+class _2D_array {
 public:
 
-  typedef _ContentT              content_type;
-  typedef _ContentT*             content_pointer;
-  typedef size_t                 size_type;
-  typedef size_t                 index_type;
-  
-  // member variables
-  content_type** _mat;
-  size_type _rows;
-  size_type _cols;
+    typedef _ContentT content_type;
+    typedef _ContentT *content_pointer;
+    typedef size_t size_type;
+    typedef size_t index_type;
 
-  explicit _2D_array(size_type _r, size_type _c)
-    : _rows {_r}, _cols {_c}
-  {
-    _mat = alloc_matrix<content_type>(_rows, _cols);
-  }
+    // member variables
+    content_type **_mat;
+    size_type _rows;
+    size_type _cols;
 
-  ~_2D_array()
-  {
-    free_matrix<content_type>(_mat, _rows, _cols);
-    _mat = nullptr;
-  }
-
-  content_type
-  operator()(index_type _i, index_type _j) const {
-    return _mat[_i][_j];
+    explicit _2D_array(size_type _r, size_type _c)
+            : _rows{_r}, _cols{_c} {
+        _mat = alloc_matrix<content_type>(_rows, _cols);
     }
 
-  content_type&
-  operator()(index_type _i, index_type _j) {
-    return _mat[_i][_j];
-  }
 
-  std::pair<size_type,size_type>
-  shape() const {
-    return std::make_pair(_rows, _cols);
-  }
+    // move constructor
+    _2D_array(_2D_array &&_m)
+            : _mat{std::move(_m._mat)}, _rows{_m._rows}, _cols{_m._cols} {
+        { _m._mat = nullptr; }
+    }
 
-  void
-  swap_rows(size_t i, size_t j)
-  {
-    std::swap(_mat[i], _mat[j]);
-  }
+    ~_2D_array() {
+        if (_mat) {
+            free_matrix<content_type>(_mat, _rows, _cols);
+        }
+        _mat = nullptr;
+    }
+
+    content_type
+    operator()(index_type _i, index_type _j) const {
+        return _mat[_i][_j];
+    }
+
+    content_type &
+    operator()(index_type _i, index_type _j) {
+        return _mat[_i][_j];
+    }
+
+    std::pair<size_type, size_type>
+    shape() const {
+        return std::make_pair(_rows, _cols);
+    }
+
+    void
+    swap_rows(size_t i, size_t j) {
+//        content_type *tmp = _mat[i];
+//        _mat[i] = _mat[j];
+//        _mat[j] = tmp;
+        std::swap(_mat[i], _mat[j]);
+    }
 
 };
 
-using Int2DArray    = _2D_array<int>;
-using Long2DArray   = _2D_array<long>;
+using Int2DArray = _2D_array<int>;
+using Long2DArray = _2D_array<long>;
 using Double2DArray = _2D_array<double>;
-using Float2DArray  = _2D_array<float>;
+using Float2DArray = _2D_array<float>;
 
-template <typename _ContentT, typename _Alloc = std::allocator<_ContentT>>
-class _2D_matrix
-{
+template<typename _ContentT, typename _Alloc = std::allocator<_ContentT>>
+class _2D_matrix {
 public:
 
-  typedef _ContentT              content_type;
-  typedef _ContentT*             content_pointer;
-  typedef size_t                 size_type;
-  typedef size_t                 index_type;
-  
-  // member variables
-  content_pointer _mat;
-  size_type _rows;
-  size_type _cols;
+    typedef _ContentT content_type;
+    typedef _ContentT *content_pointer;
+    typedef size_t size_type;
+    typedef size_t index_type;
 
-  
-  explicit _2D_matrix(size_type _r, size_type _c)
-    : _rows {_r}, _cols {_c}
-  {
-    _mat = new content_type[_rows*_cols];
-  }
+    // member variables
+    content_pointer _mat;
+    size_type _rows;
+    size_type _cols;
 
-  // copy constructor
-  _2D_matrix(const _2D_matrix& _m)
-    : _rows {_m._rows}, _cols {_m.cols}
-  {
-    _mat = new content_type[_rows*_cols];
-    std::memcpy(_mat, _m._mat, _rows*_cols*sizeof(content_type));
-  }
-    
-  // move constructor
-  _2D_matrix(_2D_matrix&& _m)
-    : _mat {std::move(_m._mat)}, _rows {_m._rows}, _cols {_m._cols}
-    { _m._mat = nullptr; }
 
-  ~_2D_matrix() {
-    if (_mat) {
-      delete[] _mat;
-    }
-    _mat = nullptr;
-  }
-
-  content_type
-  operator()(index_type _i, index_type _j) const {
-    return _mat[_i*_cols + _j];
+    explicit _2D_matrix(size_type _r, size_type _c)
+            : _rows{_r}, _cols{_c} {
+        _mat = new content_type[_rows * _cols];
     }
 
-  content_type&
-  operator()(index_type _i, index_type _j) {
-    return _mat[_i*_cols + _j];
-  }
+    // copy constructor
+    _2D_matrix(const _2D_matrix &_m)
+            : _rows{_m._rows}, _cols{_m.cols} {
+        _mat = new content_type[_rows * _cols];
+        std::memcpy(_mat, _m._mat, _rows * _cols * sizeof(content_type));
+    }
 
-  // copy assignment
-  _2D_matrix& operator=(const _2D_matrix& _m)
-  {
-    _rows = _m._rows;
-    _cols = _m._cols;
-    _mat = new content_type[_rows*_cols];
-    std::memcpy(_mat, _m._mat, _rows*_cols*sizeof(content_type));
-  }    
+    // move constructor
+    _2D_matrix(_2D_matrix &&_m)
+            : _mat{std::move(_m._mat)}, _rows{_m._rows}, _cols{_m._cols} { _m._mat = nullptr; }
 
-  // move assignment
-  _2D_matrix& operator=(_2D_matrix&& _m)
-  {
-    _rows = _m._rows;
-    _cols = _m._cols;
-    std::swap(_mat, _m._mat);
-    _m.mat = nullptr;
-  }
+    ~_2D_matrix() {
+        if (_mat) {
+            delete[] _mat;
+        }
+        _mat = nullptr;
+    }
 
-  std::pair<size_type,size_type>
-  shape() const {
-    return std::make_pair(_rows, _cols);
-  }
-  
+    content_type
+    operator()(index_type _i, index_type _j) const {
+        return _mat[_i * _cols + _j];
+    }
+
+    content_type &
+    operator()(index_type _i, index_type _j) {
+        return _mat[_i * _cols + _j];
+    }
+
+    // copy assignment
+    _2D_matrix &operator=(const _2D_matrix &_m) {
+        _rows = _m._rows;
+        _cols = _m._cols;
+        _mat = new content_type[_rows * _cols];
+        std::memcpy(_mat, _m._mat, _rows * _cols * sizeof(content_type));
+    }
+
+    // move assignment
+    _2D_matrix &operator=(_2D_matrix &&_m) {
+        _rows = _m._rows;
+        _cols = _m._cols;
+        std::swap(_mat, _m._mat);
+        _m.mat = nullptr;
+    }
+
+    std::pair<size_type, size_type>
+    shape() const {
+        return std::make_pair(_rows, _cols);
+    }
+
 };
 
-using IntMatrix    = _2D_matrix<int>;
-using LongMatrix   = _2D_matrix<long>;
+using IntMatrix = _2D_matrix<int>;
+using LongMatrix = _2D_matrix<long>;
 using DoubleMatrix = _2D_matrix<double>;
-using FloatMatrix  = _2D_matrix<float>;
+using FloatMatrix = _2D_matrix<float>;
 
 
 CTL_DEFAULT_NAMESPACE_END
--- str/distance.hpp
+++ str/distance.hpp
@@ -256,6 +256,118 @@ EditDistanceWF<T> make_wf_alg(size_t n, size_t m)
   return EditDistanceWF<T>(n, m, {1, 1, 1});
 }
 
+template<typename CostType = size_t>
+class EditDistanceBandApproxLinSpace {
+public:
+    typedef std::vector <CostType> CostVector;
+
+private:
+    _2D_array<CostType> dp_struct;
+    CostVector costs_vector;
+    size_t bandwidth;
+    size_t n;
+    size_t m;
+    const CostType Inf;
+
+public:
+    EditDistanceBandApproxLinSpace(size_t n_, size_t m_,
+                                   size_t T, const CostVector &costV)
+            : dp_struct{2, std::max(n_+1, m_+1)}, costs_vector{costV}, bandwidth{T},
+              n{n_}, m{m_},
+              Inf{(*std::max_element(costs_vector.begin(), costs_vector.end())) * 2 * (n_ + m_ + 1)} {}
+
+    void init() {
+        dp_struct(0, 0) = 0;
+        for (size_t j = 1; j <= bandwidth; ++j) {
+            dp_struct(0, j) = dp_struct(0, j - 1) + costs_vector[iI_];
+        }
+        dp_struct(1, 0) = costs_vector[iD_];
+    }
+
+    template<typename IterT>
+    CostType
+    operator()(IterT b1, IterT e1, IterT b2, IterT e2) {
+        size_t n = std::distance(b1, e1);
+        size_t m = std::distance(b2, e2);
+
+        // initialization is done here rather than in the constructor
+        // because needed at each calculation (i.e., vectors will contain
+        // values from older calculations if any)
+        init();
+
+        for (size_t i = 1; i <= n; ++i) {
+            // initialization of 'border' and 'diagonals'
+            dp_struct(1, 0) = (i <= bandwidth) ?
+                              dp_struct(0, 0) + costs_vector[iD_] : Inf;
+            if (i <= m - bandwidth) {
+                dp_struct(0, i + bandwidth) = Inf;
+            }
+            if (i >= bandwidth + 1) {
+                dp_struct(1, i - (bandwidth + 1)) = Inf;
+            }
+
+            // casts are necessary to cope with negative
+            size_t j_min = std::max<int>(1, static_cast<int>(i - bandwidth));
+            size_t j_max = std::min<int>(m, i + bandwidth);
+            for (size_t j = j_min; j <= j_max; ++j) {
+                //CostType delta{(s1[i - 1] == s2[j - 1]) ? 0 : costs_vector[iS_]};
+                CostType delta = (*(b1 + i - 1) == *(b2 + j - 1)) ? 0 : costs_vector[iS_];
+                dp_struct(1, j)
+                        = std::min<CostType>(delta + dp_struct(0, j - 1),
+                                             std::min(dp_struct(0, j) + costs_vector[iD_],
+                                                      dp_struct(1, j - 1) + costs_vector[iI_]));
+            }
+            // swap the two vectors
+           dp_struct.swap_rows(0, 1);
+        }
+        return dp_struct(0, m);
+    }
+
+//    template<typename IndexedType>
+//    CostType operator()(const IndexedType &s1, const IndexedType &s2,
+//                       size_t band) {
+//        size_t old_band = bandwidth;
+//        change_bandwidth(band);
+//        CostType result = calculate(s1, s2);
+//        change_bandwidth(old_band);
+//        return result;
+//    }
+
+    /**
+       \brief Changes the bandwidth \e without reallocating the internal
+       structure. It the new bandwidth cannot be accomodated the old
+       value is kept and \c false is returned.
+
+       \param new_band The new bandwidth
+       \return \c true if value is changed \c false otherwise
+
+       \note This is supplied for performance reasons, that is it can be
+       used to perform the algorithm for any fitting value of the
+       bandwidth without the necessity of more instances or without
+       resizing.
+     */
+    bool
+    change_bandwidth(size_t new_band) {
+        if (new_band > std::min(dp_struct._rows, dp_struct._cols)) {
+            return false;
+        }
+        bandwidth = new_band;
+        return true;
+    }
+
+
+    void print_dp_matrix() const {
+        dp_struct.print_matrix();
+    }
+
+}; // EditDistanceBandApproxLinSpace
+
+
+template<typename CostT = size_t>
+EditDistanceBandApproxLinSpace<CostT> make_band_linear_alg(size_t n, size_t m, size_t T) {
+    return EditDistanceBandApproxLinSpace<CostT>(n, m, T, {1, 1, 1});
+}
+
 
 CTL_DEFAULT_NAMESPACE_END
 
